d17acf298d38fbdca5abaf2f2d56cdd12ee1758e
Amogh Hosamane <164329830+Amoghhosamane@users.noreply.github.com>
2025-12-22 01:53:25 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.imgur.com",
-      },
-      {
-        protocol: "https",
-        hostname: "img.youtube.com",
-      },
-      {
-        protocol: "https",
-        hostname: "googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-    ],
-  },
   reactStrictMode: false,
 };
 
